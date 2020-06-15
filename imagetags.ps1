6ede1de732a994d2c7abbdb21b032b3bf0772074
Micheal Ieti <micheal.ieti@xero.com>
2020-06-15 22:57:14 +1000

    Add images and stuff

---
+++ imagetags.ps1
@@ -0,0 +1,5 @@
+﻿# Write-Output '<img src="../img/weddingphotos/f1/Herah _ Michael-01.jpg" width="120px">'
+
+Get-ChildItem -Name | ForEach-Object {
+    Write-Output "<img src=`"../img/weddingphotos/f1/$($_)`">"
+} | clip
