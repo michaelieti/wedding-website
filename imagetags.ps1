17f3ecc74c7d9bc65cd3966339427c4f31c0803c
Micheal Ieti <micheal.ieti@xero.com>
2020-06-17 17:10:30 +1000

    Added links and thumbnails

--- imagetags.ps1
+++ imagetags.ps1
@@ -1,5 +1,5 @@
 ﻿# Write-Output '<img src="../img/weddingphotos/f1/Herah _ Michael-01.jpg" width="120px">'
 
 Get-ChildItem -Name | ForEach-Object {
-    Write-Output "<img src=`"../img/weddingphotos/f1/$($_)`">"
+    Write-Output "         <a target=`"_blank`" href=`"../img/weddingphotos/f1/$($_)`"><img src=`"../img/weddingphotos/f1/thumbnails/$($_)`"></a>"
 } | clip
